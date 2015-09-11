d603622c42aa0dd92c4e347c289783467d7311e7
warreq <brandon.s.lucas@gmail.com>
2015-09-11 11:51:22 +0000

    implement mustache template rendering. Fixes #25

--- src/main/java/edu/dirtybit/battlechat/controller/SessionController.java
+++ src/main/java/edu/dirtybit/battlechat/controller/SessionController.java
@@ -1,14 +1,22 @@
 package edu.dirtybit.battlechat.controller;
 
+import java.util.HashMap;
+import java.util.Map;
 import static spark.Spark.*;
+import spark.ModelAndView;
+import spark.template.mustache.MustacheTemplateEngine;
 
 public class SessionController implements Controller {
 
     public void buildRoutes() {
-           get("/hello", (req, res) -> sayHello());
+        get("/hello", (req, res) -> renderTemplate());
+        post("/", (req, res) -> renderTemplate());
     }
 
-    private String sayHello() {
-        return "Hello Heroku";
+    private String renderTemplate() {
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Test Bro");
+        MustacheTemplateEngine engine = new MustacheTemplateEngine();
+        return engine.render(new ModelAndView(map, "example.mustache"));
     }
 }
