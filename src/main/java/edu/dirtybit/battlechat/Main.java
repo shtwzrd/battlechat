c9f007ba26125ba6395b69f48bee829cdba787eb
warreq <brandon.s.lucas@gmail.com>
2015-09-21 13:27:40 +0000

    restructure webapp again. Serving files on '/' not cool apparently

--- src/main/java/edu/dirtybit/battlechat/Main.java
+++ src/main/java/edu/dirtybit/battlechat/Main.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        staticFileLocation("/");
+        staticFileLocation("/app");
         port(getHerokuAssignedPort());
         activateControllers(collectControllers());
     }
---
+++ src/main/resources/app/js/app.socket.js
@@ -0,0 +1,56 @@
+(function( appSocket, $, undefined ) {
+    // Private property
+
+    var webSocket;
+    var messages = document.getElementById("messages");
+    var domain = $(this.document).attr("baseURI").split('/')[2];
+
+    // Public property
+
+    appSocket.id = $(this.document).attr("URL").split('/').pop();
+
+    // Public functions
+
+    appSocket.openSocket = function () {
+        // Ensures only one connection is open at a time
+        if(webSocket !== undefined && webSocket.readyState !== WebSocket.CLOSED){
+            writeResponse("WebSocket is already opened.");
+            return;
+        }
+        // Create a new instance of the websocket
+        webSocket = new WebSocket("ws://" + domain + "/session", ["v1", appSocket.id]);
+
+        // Binds functions to the listeners for the websocket.
+        webSocket.onopen = function(event){
+            if(event.data === undefined)
+                return;
+
+            writeResponse(event.data);
+        };
+
+        webSocket.onmessage = function(event){
+            writeResponse(event.data);
+        };
+
+        webSocket.onclose = function(event){
+            writeResponse("Connection closed");
+        };
+    };
+
+    // Sends the value of the text input to the server
+    appSocket.send = function (){
+        var text = document.getElementById("messageinput").value;
+        webSocket.send(text);
+    };
+
+    appSocket.closeSocket = function (){
+        webSocket.close();
+    };
+
+    // Private function
+
+    function writeResponse(text){
+        messages.innerHTML += "<br/>" + text;
+    };
+
+}( window.appSocket = window.appSocket || {}, jQuery ));
